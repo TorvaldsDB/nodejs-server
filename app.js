b9085c04cf043244331b21ffae5659af8528be3b
torvalds <deandb2015@gmail.com>
2024-12-25 22:45:04 +0800

    Import Express

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-import http from "http";
-import routes from "./routes.js";
+import bodyParser from "body-parser";
+import express from "express";
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000, (error) => {
-  if (error) {
-    return console.log("Something went wrong:", error);
-  }
-  console.log("Server is running on port 3000");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/add-product", (req, res, next) => {
+  console.log("Second middleware");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
 });
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+// middleware
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(3000);
