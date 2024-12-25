53bec063e9427843f31316ab8974b0914c6ceab2
torvalds <deandb2015@gmail.com>
2024-12-25 10:07:39 +0800

    Finish vanilla Node.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import routes from "./routes.js";
 
 const server = http.createServer(routes);
 
-server.listen(3000, () => {
+server.listen(3000, (error) => {
+  if (error) {
+    return console.log("Something went wrong:", error);
+  }
   console.log("Server is running on port 3000");
 });
