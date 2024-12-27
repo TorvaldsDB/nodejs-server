28178e44c07e374078b5ddcd417809c69e400c84
torvalds <deandb2015@gmail.com>
2024-12-27 14:38:10 +0800

    Add 404 page

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import bodyParser from "body-parser";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import adminRoutes from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -11,7 +16,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
