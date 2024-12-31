0d5ee0e77d3436df0acf068a12eb49d6b6cb1cec
torvalds <deandb2015@gmail.com>
2024-12-31 18:54:54 +0800

    Implement express-handlebars

--- app.js
+++ app.js
@@ -4,12 +4,20 @@ const bodyParser = require("body-parser");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-const rootDir = require("./util/path");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHbs.engine({
+    layoutsDir: "views/layouts",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+// app.set("view engine", "pug");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
