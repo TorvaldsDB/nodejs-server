266a92a69d67f394829a8293f0b98a07bb190270
torvalds <deandb2015@gmail.com>
2025-01-04 22:55:20 +0800

    Implement controllers

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const errorController = require("./controllers/error");
 
 const app = express();
 
@@ -13,12 +14,10 @@ app.set("views", "views");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorController.get404);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
